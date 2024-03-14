ca63550cbfa34b5cacec98a277bc679a2dd1b856
VasVL <sobakagman.com@gmail.com>
2024-03-15 00:06:36 +0300

    Кстати, коллаписирующему заголовку нельзя подзаголовок приделать. Ещё и тут прблемы

--- app/src/main/java/com/example/to_dolist/ui/allDealsScreen/AllDealsAdapter.kt
+++ app/src/main/java/com/example/to_dolist/ui/allDealsScreen/AllDealsAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.to_dolist.ui.allDealsScreen
 
-import android.icu.util.Calendar
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
