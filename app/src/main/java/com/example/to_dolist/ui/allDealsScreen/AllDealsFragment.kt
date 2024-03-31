36d1f56fd516081a68a9c2eb43a4f702d7f6ed1b
VasVL <sobakagman.com@gmail.com>
2024-03-31 18:58:06 +0300

    Пытаюсь сделать так, чтоб системный бар не перекрывал кнопку, как в документации написано. На эмуляторе этот слушатель срабатывает, а на телефоне нет :(

--- app/src/main/java/com/example/to_dolist/ui/allDealsScreen/AllDealsFragment.kt
+++ app/src/main/java/com/example/to_dolist/ui/allDealsScreen/AllDealsFragment.kt
@@ -1,11 +1,17 @@
 package com.example.to_dolist.ui.allDealsScreen
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -52,13 +58,30 @@ class AllDealsFragment : Fragment() {
             appBar.addOnOffsetChangedListener { appBarLayout, offset ->
                 val collapsingToolbar = collapsingToolbar.height
                 val toolbar = toolbar.height
-                val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
-                val size = resources.getDimensionPixelSize(resourceId)
+                val insets = ViewCompat.getRootWindowInsets(appBarLayout)?.getInsets(WindowInsetsCompat.Type.systemBars())
+                val size = insets?.top ?: 0
                 if (collapsingToolbar + offset == size + toolbar) {
                     toolbarShadow.visibility = View.VISIBLE
                 } else {
                     toolbarShadow.visibility = View.INVISIBLE
                 }
+            }
+
+            /** Это, чтоб SystemBar не перкрывал fab
+             * Только на моём телефоне не работает чё-то... */
+            ViewCompat.setOnApplyWindowInsetsListener(addButton) { v, windowInsets ->
+                val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+                Log.d("123","insets $insets")
+                textDone.text = insets.toString()
+                Log.d("123","v $v")
+                v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                    bottomMargin = insets.bottom
+                    leftMargin = insets.left
+                    rightMargin = insets.right
+                }
+
+                WindowInsetsCompat.CONSUMED
             }
         }
 
